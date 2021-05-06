16fdfacc77320a0a53a6fa2c0c8d72e9458d9449
Oli <olifer97@gmail.com>
2021-05-05 21:23:28 -0300

    fix meetsCondition

--- miner/miner.py
+++ miner/miner.py
@@ -8,17 +8,16 @@ class Miner:
       print("Create miner")
 
     def meetsCondition(self, block):
-        difficulty = block.difficulty() - 1
-        return True if difficulty == 0 else block.hash() < (2**256) / difficulty
+      return block.hash() < (2**256) / block.difficulty()
 
     def mine(self, block):
-        timestamp = datetime.datetime.now()
-        block.setTimestamp(timestamp)
-        while not self.meetsCondition(block):
-            block.addNonce()
-            block.setTimestamp(datetime.datetime.now())
+      timestamp = datetime.datetime.now()
+      block.setTimestamp(timestamp)
+      while not self.meetsCondition(block):
+          block.addNonce()
+          block.setTimestamp(datetime.datetime.now())
 
-        # try to save block in blockchain manager
-        # send hash to miner manager to notify succes or failure
+      # try to save block in blockchain manager
+      # send hash to miner manager to notify succes or failure
 
-        return block.hash()
+      return block.hash()
