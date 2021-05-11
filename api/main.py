b9d0722ab3cb7421cabd0c7be051039f4d30b8c6
Oli <olifer97@gmail.com>
2021-05-10 21:20:05 -0300

    parse config

--- api/main.py
+++ api/main.py
@@ -10,40 +10,61 @@ import logging
 import sys
 import os
 
-N_MINERS = int(os.environ["N_MINERS"])
-BLOCKCHAIN_HOST = os.environ["BLOCKCHAIN_IP"]
-WRITER_PORT = int(os.environ["WRITER_PORT"])
 
-READER_PORT = int(os.environ["READER_PORT"])
+def parse_config_params():
+    """ Parse env variables to find program config params
 
-N_WORKERS = int(os.environ["N_WORKERS"])
-API_PORT = int(os.environ["API_PORT"])
-API_LISTENERS = int(os.environ["API_LISTENERS"])
+    Function that search and parse program configuration parameters in the
+    program environment variables. If at least one of the config parameters
+    is not found a KeyError exception is thrown. If a parameter could not
+    be parsed, a ValueError is thrown. If parsing succeeded, the function
+    returns a map with the env variables
+    """
+    config_params = {}
+    try:
+        config_params["api_port"] = int(os.environ["API_PORT"])
+        config_params["reader_port"] = int(os.environ["READER_PORT"])
+        config_params["writer_port"] = int(os.environ["WRITER_PORT"])
+        config_params["listen_backlog"] = int(os.environ["API_LISTENERS"])
+        config_params["n_workers"] = int(os.environ["N_WORKERS"])
+        config_params["n_miners"] = int(os.environ["N_MINERS"])
+        config_params["threshold"] = int(os.environ["CHUNKS_THRESHHOLD"])
+        config_params['blockchain_host'] = os.environ["BLOCKCHAIN_IP"]
+    except KeyError as e:
+        raise KeyError(
+            "Key was not found. Error: {} .Aborting server".format(e))
+    except ValueError as e:
+        raise ValueError(
+            "Key could not be parsed. Error: {}. Aborting server".format(e))
 
-THRESHHOLD = int(os.environ["CHUNKS_THRESHHOLD"])
+    return config_params
+
 
 def main():
+    config = parse_config_params()
 
-    chunks_queue = queue.Queue(maxsize=THRESHHOLD)
+    chunks_queue = queue.Queue(maxsize=config['threshold'])
     query_queue = queue.Queue()
     stats_queue = queue.Queue()
     response_queue = queue.Queue()
 
-    miners_handler = MinersHandler(N_MINERS, chunks_queue, stats_queue, (BLOCKCHAIN_HOST, WRITER_PORT))
+    miners_handler = MinersHandler(config['n_miners'], chunks_queue, stats_queue, (
+        config['blockchain_host'], config['writer_port']))
     miners_handler.start()
 
-    query_handler = QueryHandler(N_MINERS, query_queue, stats_queue, response_queue, (BLOCKCHAIN_HOST, WRITER_PORT), (BLOCKCHAIN_HOST, READER_PORT))
+    query_handler = QueryHandler(config['n_miners'], query_queue, stats_queue, response_queue, (
+        config['blockchain_host'], config['writer_port']), (config['blockchain_host'], config['reader_port']))
     query_handler.start()
 
-    request_handler = RequestHandler(API_PORT, API_LISTENERS, chunks_queue, query_queue, response_queue, N_WORKERS)
+    request_handler = RequestHandler(
+        config['api_port'], config['listen_backlog'], chunks_queue, query_queue, response_queue, config['n_workers'])
     request_handler.run()
 
-    
 
 if __name__ == "__main__":
     logging.basicConfig(
-		format='%(asctime)s %(levelname)-8s %(message)s',
-		level=logging.INFO,
-		datefmt='%Y-%m-%d %H:%M:%S',
-	)
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
     main()
