7d5d0ba3a10890052023924dc905936f5f520ac5
Oli <olifer97@gmail.com>
2021-05-22 16:06:35 -0300

    add port and host by env in clients

--- client.py
+++ client.py
@@ -1,43 +1,59 @@
-import threading
 import json
-import socket
-import sys, getopt
+import sys
+import getopt
+import os
 
 from common.constants import *
 from common.custom_socket.client_socket import ClientSocket
 
 BLOCKCHAIN_ADDRESS = ('127.0.0.1', 5000)
-RESPONSE_SIZE = 1024
 
 OPTIONS = 'client.py [--chunk <chunk>] [--block <hash>] [--timestamp <timestamp>] [--stats]'
 
 
 def send_and_recv(request, address):
-    sock = ClientSocket(address = address)
-    sock.send_with_size(json.dumps(request))
-    response = sock.recv_with_size()
-    sock.close()
-    return response 
+   sock = ClientSocket(address=address)
+   sock.send_with_size(json.dumps(request))
+   response = sock.recv_with_size()
+   sock.close()
+   return response
 
 
-def saveChunk(chunk):
-    request = {"type": POST_CHUNK, "parameter": chunk}
-    return send_and_recv(request, BLOCKCHAIN_ADDRESS)
+def saveChunk(chunk, config):
+   request = {"type": POST_CHUNK, "parameter": chunk}
+   return send_and_recv(request, (config['host'], config['port']))
 
-def getBlock(hash):
-    request = {"type": GET_BLOCK, "parameter": hash}
-    return send_and_recv(request, BLOCKCHAIN_ADDRESS)
 
-def getBlocks(timestamp):
-    request = {"type": GET_BLOCKS, "parameter": timestamp}
-    return send_and_recv(request, BLOCKCHAIN_ADDRESS)
+def getBlock(hash, config):
+   request = {"type": GET_BLOCK, "parameter": hash}
+   return send_and_recv(request, (config['host'], config['port']))
 
-def getStats():
-    request = {"type": GET_STATS}
-    return send_and_recv(request, BLOCKCHAIN_ADDRESS)
 
+def getBlocks(timestamp, config):
+   request = {"type": GET_BLOCKS, "parameter": timestamp}
+   return send_and_recv(request, (config['host'], config['port']))
+
+
+def getStats(config):
+   request = {"type": GET_STATS}
+   return send_and_recv(request, (config['host'], config['port']))
+
+
+def parse_config_params():
+   config_params = {}
+   try:
+      config_params["host"] = os.environ["HOST"]
+   except KeyError as e:
+      config_params["host"] = BLOCKCHAIN_ADDRESS[0]
+
+   try:
+      config_params["port"] = int(os.environ["PORT"])
+   except KeyError as e:
+      config_params["port"] = BLOCKCHAIN_ADDRESS[1]
+   return config_params
 
 def main(argv):
+   config = parse_config_params()
    try:
       opts, args = getopt.getopt(argv,"hc:b:t:s",["chunk=","block=", "timestamp="])
    except getopt.GetoptError:
@@ -48,19 +64,19 @@ def main(argv):
          print(OPTIONS)
          sys.exit()
       elif opt in ("-c", "--chunk"):
-         print(saveChunk(arg))
+         print(saveChunk(arg, config))
          sys.exit()
       elif opt in ("-b", "--block"):
-         print(getBlock(arg))
+         print(getBlock(arg, config))
          sys.exit()
       elif opt in ("-t", "--timestamp"):
-         print(getBlocks(arg))
+         print(getBlocks(arg, config))
          sys.exit()
       elif opt in ("-s", "--stats"):
-         print(getStats())
+         print(getStats(config))
          sys.exit()
     
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+   main(sys.argv[1:])
--- multiple_clients.py
+++ multiple_clients.py
@@ -1,6 +1,6 @@
 import threading
 import json
-import socket
+import os
 import sys
 import getopt
 import queue
@@ -9,7 +9,6 @@ from common.constants import *
 from common.custom_socket.client_socket import ClientSocket
 
 BLOCKCHAIN_ADDRESS = ('127.0.0.1', 5000)
-RESPONSE_SIZE = 1024
 
 OPTIONS = 'client.py [--clients <number>] [--size <number>] [--requests <number>]'
 
@@ -31,18 +30,33 @@ class Client(threading.Thread):
       self.request_queue = request_queue
 
     def run(self):
+      global stop_threads
       while True:
-        request = self.request_queue.get()
-        self.request_queue.task_done()
-        print(send_and_recv(request, self.address))
-        global stop_threads
-        if stop_threads:
+        try:
+          request = self.request_queue.get()
+          self.request_queue.task_done()
+          print(send_and_recv(request, self.address))
+          if stop_threads:
             break
-        # close(sock)
+        except queue.Empty:
+          if stop_threads:
+            break
 
+def parse_config_params():
+   config_params = {}
+   try:
+      config_params["host"] = os.environ["HOST"]
+   except KeyError as e:
+      config_params["host"] = BLOCKCHAIN_ADDRESS[0]
 
+   try:
+      config_params["port"] = int(os.environ["PORT"])
+   except KeyError as e:
+      config_params["port"] = BLOCKCHAIN_ADDRESS[1]
+   return config_params
 
 def main(argv):
+  config = parse_config_params()
   requests_queue = queue.Queue()
   try:
     opts, args = getopt.getopt(argv,"hc:r:s:")
@@ -60,7 +74,7 @@ def main(argv):
     elif opt in ("-s", "--chunk_size"):
       chunk_size = arg
 
-  clients_threads = [Client(BLOCKCHAIN_ADDRESS, requests_queue) for i in range(int(clients))]
+  clients_threads = [Client((config['host'], config['port']), requests_queue) for i in range(int(clients))]
   for client in clients_threads:
       client.start()
 
@@ -79,4 +93,4 @@ def main(argv):
   sys.exit()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+  main(sys.argv[1:])
